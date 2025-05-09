d81e81776174af4d1fa85c9a09464c3792813e0d
mafutka <mafutka@gmail.com>
2025-05-09 22:12:32 +0300

    task-6-edited

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,11 +2,11 @@ function getFirstElement<T>(arr: T[]): T {
     return arr[0];
   }
   
- console.log(getFirstElement([1, 2, 3]));
+ console.log(getFirstElement<number>([1, 2, 3]));
  
- console.log(getFirstElement(["a", "b", "c"]));
+ console.log(getFirstElement<string>(["a", "b", "c"]));
  
- console.log(getFirstElement([true, false, true]));
+ console.log(getFirstElement<boolean>([true, false, true]));
  
 //   Завдання:
 
